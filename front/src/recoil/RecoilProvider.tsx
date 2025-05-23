07226fe6c44f93b15a8d50801627101dae5fbc78
wooseok <youkn0wthat@naver.com>
2025-05-24 03:07:22 +0900

    : feat : Theme 모드 임시 적용

--- front/src/recoil/RecoilProvider.tsx
+++ front/src/recoil/RecoilProvider.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "next-themes";
 import type { ReactNode } from "react";
 
 const RecoilProvider = ({ children }: { children: ReactNode }) => (
-  <RecoilRoot>{children}</RecoilRoot>
+  <RecoilRoot>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </ThemeProvider>
+  </RecoilRoot>
 );
 
 export default RecoilProvider;
