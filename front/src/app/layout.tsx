55ceaad064199a8188a9f8fc7fe86f77992cac4a
wooseok <youkn0wthat@naver.com>
2025-05-24 00:05:14 +0900

    : feat : RecoilProvider 생성 및 레이아웃 연결

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
+import RecoilProvider from "@/recoil/RecoilProvider";
 
 const notoSans = Noto_Sans_KR({
   subsets: ["latin"],
@@ -22,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${notoSans.className} antialiased ml-[476px]`}>
-        {children}
+        <RecoilProvider>{children}</RecoilProvider>
       </body>
     </html>
   );
