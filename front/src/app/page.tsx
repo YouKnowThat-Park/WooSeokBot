266072ec23f4579fd361cd489b295998a81c75b9
wooseok <youkn0wthat@naver.com>
2025-05-28 02:19:07 +0900

    : feat : 페이지 로드 시 안내사항 모달로 전달

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./home/Home";
+import NoticeModal from "./_components/NoticeModal";
 
 const page = () => {
   return (
     <div className="">
       <Home />
+      <NoticeModal />
     </div>
   );
 };
