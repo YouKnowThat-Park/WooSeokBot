8efdab217c253b9ad1649beb954a14d025291177
wooseok <youkn0wthat@naver.com>
2025-05-29 01:05:17 +0900

    : style :  Layout inner 값 변경

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body
-        className={`${notoSans.className} antialiased  bg-white dark:bg-[#111111] ml-[476px]`}
+        className={`${notoSans.className} antialiased  bg-white dark:bg-[#111111] ml-[500px]`}
       >
         <RecoilProvider>
           {children}
