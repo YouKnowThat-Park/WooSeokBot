164f733c21be9dc3c24acbdd0e4de92a1c0e3703
wooseok <youkn0wthat@naver.com>
2025-06-14 19:52:55 +0900

    : feat : Front DB 생성

---
+++ front/src/data/WooseokBotTechItems.ts
@@ -0,0 +1,58 @@
+export const WooseokBotTechItems = [
+  {
+    category: "Front-end",
+    tech: "React",
+    reason: "컴포넌트 기반 구조로 사용성과 유지보수성 뛰어나 UI 개발에 유리",
+  },
+  {
+    category: "Front-end",
+    tech: "Next.js",
+    reason: "라우팅, 성능 최적화 기능 포함 ",
+  },
+  {
+    category: "Language",
+    tech: "TypeScript",
+    reason: "정적 타입 검사로 안정성 확보, 코드 오류 줄임",
+  },
+  {
+    category: "Back-end",
+    tech: "Django",
+    reason: "Ai와 높은 호환성",
+  },
+  {
+    category: "DB",
+    tech: "PostGreSQL",
+    reason:
+      "복잡한 사용자 대화 및 로그 데이터를 구조화하여 안정적으로 관리하기 위함",
+  },
+  {
+    category: "Library",
+    tech: "Recoil",
+    reason: "간단하게 상태 관리 가능, Redux보다 사용성 높음",
+  },
+  {
+    category: "Library",
+    tech: "React-Query",
+    reason: "비동기 데이터 처리, 캐싱, 리페치 등 안정적인 API 연동",
+  },
+  {
+    category: "Library",
+    tech: "Yup",
+    reason: "스키마 기반 validation으로 안정성 확보",
+  },
+  {
+    category: "Library",
+    tech: "Framer-Motion",
+    reason: "자유로운 애니메이션 구현 및 전환 제어",
+  },
+  {
+    category: "API",
+    tech: "GPT 4o mini",
+    reason: "3.5turbo 보다 저렴하고 빠르며 똑똑함",
+  },
+  {
+    category: "Styling",
+    tech: "Tailwind CSS",
+    reason: "빠른 개발과 유지보수, 스타일드 컴포넌트보다 간결",
+  },
+];
