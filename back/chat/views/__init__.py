8fc726b51da06ddd5a035af1e1bf165b039da93e
wooseok <youkn0wthat@naver.com>
2025-06-26 03:38:33 +0900

    : fix Back : CORS 에러 수정

--- back/chat/views/__init__.py
+++ back/chat/views/__init__.py
@@ -1,2 +1,10 @@
-from .chat import create_chat, get_chat, chat_ask
+from .chat import create_chat, get_chat, chat_ask, chat_ask_by_slug
 from .feedback import submit_feedback
+
+__all__ = [
+    "create_chat",
+    "get_chat",
+    "chat_ask",
+    "chat_ask_by_slug",
+    "submit_feedback",
+]
