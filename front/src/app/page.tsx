bb6c6e251b3d4babcc47474005f055557dc6bb6d
wooseok <youkn0wthat@naver.com>
2025-06-25 13:46:33 +0900

    : remove : 서비스 준비 중 모달 삭제 (배포 직전)

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Home from "./home/Home";
-import NoticeModal from "./_components/NoticeModal";
 
 const page = () => {
   return (
     <div className="flex justify-end items-end mr-[450px]">
-      <NoticeModal />
       <Home />
     </div>
   );
